95bb6003e743a08008be712eb3bdc7b317e7befd
HomeKondo <nkcgta.hikaru.kondo@gmail.com>
2020-12-14 02:56:57 +0900

    不要ファイル削除、ハンバーガーメニューの仮追加

--- KondoPortfolio/public/assets/js/common.js
+++ KondoPortfolio/public/assets/js/common.js
@@ -2,14 +2,13 @@
 document.write("<p>common.js</p>");
 
 
-var elem = document.getElementById('elem');
+var elem = document.getElementById('menu');
 anime({
   targets: elem,
   translateX: 250
-  easing: 'easeOutCubic'
 })
 
-var test = document.getElementById('test');
+var test = document.getElementById('menuitem');
 anime({
   targets: test,
   translateX: 250
