54d7ea22b172f9c490663b6aee3605ba471c0c57
HikaruKondo <nkcgta.hikaru.kondo@gmail.com>
2020-12-17 14:54:15 +0900

    メニュー動作仮追加

--- KondoPortfolio/public/assets/js/common.js
+++ KondoPortfolio/public/assets/js/common.js
@@ -1,15 +1,35 @@
 
-document.write("<p>common.js</p>");
+var isOpenMenu = false;
 
+$("#hoge").click(function(){
+  //クリック後の処理
+  //$("#hoge").hide();
+  if(!isOpenMenu){
+    $('#hoge').attr('src', 'assets/img/top/frame_hamburger_out.png');
+    var elem = document.getElementById('menuitem');
+    anime({
+      targets: elem,
+      translateX: 250
+    })
+    isOpenMenu = true;
+  }
+  else if(isOpenMenu){
+    $('#hoge').attr('src', 'assets/img/top/frame_hamburger.png');
+    var elem = document.getElementById('menuitem');
+    anime({
+      targets: elem,
+      translateX: -250
+    })
+    isOpenMenu = false;
+  }
+});
 
-var elem = document.getElementById('menu');
-anime({
-  targets: elem,
-  translateX: 250
-})
+function MenuControl(test){
 
-var test = document.getElementById('menuitem');
-anime({
-  targets: test,
-  translateX: 250
-})
+}
+
+// var elem = document.getElementById('menuitem');
+// anime({
+//   targets: elem,
+//   translateX: 250
+// })
